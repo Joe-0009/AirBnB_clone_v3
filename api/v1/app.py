e35100ea6730f49f70d78e361605729a483b3bc0
Joe-0009 <youssefrachidi1994@gmail.com>
2024-06-06 14:26:14 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from models import storage
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,7 +15,11 @@ def teardown_appcontext(exception):
     """
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404 
+
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, thread=True)
+    app.run(host=host, port=port, threaded=True)
