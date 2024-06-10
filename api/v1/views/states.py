dc8d3df4c07177cd456860d0fe49f672d48a8864
Joe-0009 <youssefrachidi1994@gmail.com>
2024-06-10 01:41:21 -0700

    gg

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,7 @@ Flask route that returns json status response
 """
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
-from models import storage, CNC
+from models import storage
 from models.state import State
 
 @app_views.route('/states', methods=['GET', 'POST'])
